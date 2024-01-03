aeb12e11664df95b555caf1abe2c7ff98403fe65
FT/Fati <fatima.amkachou@gmail.com>
2024-01-03 12:50:13 -0800

    mandotary

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -27,16 +27,16 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-        size_t left_size = 0;
-        size_t right_size = 0;
+	size_t left_size = 0;
+	size_t right_size = 0;
 
-        if (!tree)
-                return (0);
+	if (!tree)
+		return (0);
 
-        left_size += 1 + binary_tree_height(tree->left);
-        right_size += 1 + binary_tree_height(tree->right);
-        if (left_size > right_size)
-                return (left_size);
+	left_size += 1 + binary_tree_height(tree->left);
+	right_size += 1 + binary_tree_height(tree->right);
+	if (left_size > right_size)
+		return (left_size);
 
-        return (right_size);
+	return (right_size);
 }
