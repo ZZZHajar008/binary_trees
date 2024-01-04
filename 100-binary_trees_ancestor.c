5234c2b9e55fba97a6986ba1fb28eb93caa224e5
ZZZHajar008 <ilyass.infinite345@gmail.com>
2024-01-04 21:57:47 +0100

    binary tree

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -2,11 +2,14 @@
 
 /**
  * binary_trees_ancestor - Finds the lowest common ancestor of two nodes.
- * @first: Pointer to the first node.
- * @second: Pointer to the second node.
  *
- * Return: If no common ancestors return NULL, else return common ancestor.
+ * @first: A pointer to the first node.
+ * @second: A pointer to the second node.
+ *
+ * Return: A pointer to the lowest common ancestor node of the two given nodes.
+ *         If no common ancestor was found, your function must return NULL.
  */
+
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 		const binary_tree_t *second)
 {
